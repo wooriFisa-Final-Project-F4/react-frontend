923df214d8c6ae60d9baa1f828692dc687fb2d8a
Jiwoon <kfromh0136@Naver.com>
2023-08-14 19:10:28 +0900

    [#11] [Style] Mobile-UI 구성
    모바일에서 보여지는 UI를 구성하였다.
    상단에 있던 아이콘과 네브바에 있던 메뉴를 푸터바로 바꿔주었고, 모바일 구성화면에서 보여지는 것들을 확인하고 스타일을 수정해주었다.

--- src/Home/Footer/Footer.tsx
+++ src/Home/Footer/Footer.tsx
@@ -1,7 +1,63 @@
 import React from "react";
-
-const Footer = () => {
-  return <div></div>;
+import {
+  FavoriteBorderOutlined,
+  NotificationsNone,
+  PersonOutline,
+  ShoppingBagOutlined,
+  HomeOutlined,
+} from "@mui/icons-material";
+import "./Footer.css";
+import { Link } from "react-router-dom";
+export const Footer = () => {
+  return (
+    <section className="footer">
+      <div className="icons">
+        <div className="icon">
+          <Link to="/mypage">
+            <i>
+              <PersonOutline className="footer_icon" />
+            </i>
+          </Link>
+        </div>
+        <div className="noti">
+          <Link to="/cart">
+            <i>
+              <NotificationsNone className="footer_icon" />
+            </i>
+            <span>0</span>
+          </Link>
+        </div>
+        <div className="icon">
+          <Link to="/">
+            <i>
+              <HomeOutlined
+                style={{
+                  fontSize: "40px",
+                  color: "black",
+                  backgroundColor: "white",
+                  borderRadius: "50px",
+                }}
+              />
+            </i>
+            <span>0</span>
+          </Link>
+        </div>
+        <div className="icon">
+          <Link to="/cart">
+            <i>
+              <FavoriteBorderOutlined className="footer_icon" />
+            </i>
+          </Link>
+        </div>
+        <div className="icon">
+          <Link to="/cart">
+            <i>
+              <ShoppingBagOutlined className="footer_icon" />
+            </i>
+            <span>0</span>
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
 };
-
-export default Footer;
